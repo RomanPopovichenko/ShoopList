8ab53422114bc1f5ed87a08d6c0d2c62e4027a34
roman <aregsa2@gmail.com>
2023-02-11 21:29:05 +0200

    was added activity for add shop item

---
+++ app/src/main/java/com/example/shoplist/Presentation/AddShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoplist.Presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoplist.R
+
+class AddShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_shop_item)
+    }
+}
